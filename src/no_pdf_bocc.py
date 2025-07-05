72eb0c8d31f851aa8b1d11689ec7c03929510f11
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-07-06 00:10:20 +0200

    feat(download): fusion des PDFs BOCC, ajout des logs détaillés et mesure précise du temps de traitement par article et global

--- src/no_pdf_bocc.py
+++ src/no_pdf_bocc.py
@@ -6,6 +6,9 @@ import cloudscraper
 from urllib.parse import urljoin
 import time
 import random
+from PyPDF2 import PdfReader, PdfWriter
+import io
+from typing import TextIO
 
 
 
@@ -79,3 +82,123 @@ def get_all_pdfs_contents(download_pages : list, file_name : str) :
     return pdf_binary_content, failed_pdf_download, file_path
 
     
+###########################################################################
+
+# enregistrer le pdf
+    
+def write_binary_to_pdf(pdf_binary_content : list, file_path : str) :
+    
+    writer = PdfWriter()
+    for pdf_bytes in pdf_binary_content :
+        reader = PdfReader(io.BytesIO(pdf_bytes))
+        for page in reader.pages :
+            writer.add_page(page)
+    output_bytes = io.BytesIO()
+    writer.write(output_bytes)
+    pdf_concatenate = output_bytes.getvalue()
+    
+    with open(file_path, "wb") as f : 
+        f.write(pdf_concatenate)
+    
+    
+###########################################################################
+
+# fonction pour enregistrer les logs, pour vérifier les echecs
+
+def log_and_print(message : str, logfile : TextIO) : 
+    print(message)
+    logfile.write(message + "\n")
+    logfile.flush()
+
+
+###########################################################################
+
+# chronometre pour voir combien de temps le traitement prend
+
+def chronometre() :
+    
+    start_time = time.perf_counter()
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    return 
+    
+    
+    pass
+
+
+
+
+
+
+###########################################################################
+    
+# itérer et ajouter de la récursion pour tout récupérer
+
+def iterate_all_untill_all_downloaded(data :list[dict], max_attempts: int = 3) : 
+    
+    # début du chronomètre
+    start_time = time.perf_counter()
+    
+    # ouvrir le fichier log dès le départ pour enregistrer les logs
+    with open ("scrapping_bocc.log", "a", encoding="utf-8") as logfile :
+        log_and_print("\n"+"="*50+"  DÉBUT DE TRAITEMENT  "+"="*50+"\n"
+                      +"="*43+f"  NOMBRE D'ARTICLES A TRAITER : {len(data)}  "+"="*43+"\n",
+                      logfile)
+        
+        for article in data : 
+        # for article in data[:3] :       # test
+            article_start_time = time.perf_counter()
+            
+            # extraire les liens de téléchargement des pdf et le nom du bocc de chaque page
+            download_pages, file_name = get_download_page_list(article)
+            log_and_print(f"Début traitement article : {file_name} ({len(download_pages)} PDFs attendus)", logfile)
+            
+            # tenter de récupérer tous les contenus des fichiers pdf
+            attempts = 0
+            current_download_pages = download_pages
+            pdf_binary_content = []
+            
+            while current_download_pages and attempts < max_attempts : 
+                attempts += 1
+                log_and_print(f"Tentative {attempts}/{max_attempts} pour {file_name} ({len(current_download_pages)} PDFs à télécharger)", logfile)
+                
+                pdf_contents, failed_pdf_download, file_path = get_all_pdfs_contents(current_download_pages, file_name)
+                pdf_binary_content.extend(pdf_contents)
+                
+                if not failed_pdf_download :
+                    log_and_print(f"Tous les PDFs pour '{file_name}' téléchargés avec succès.", logfile)
+                    break
+                
+                elif len(failed_pdf_download) == len(current_download_pages) : 
+                    log_and_print(f"Aucun progrès pour '{file_name}', arrêt des tentatives.", logfile)
+                    break
+                
+                else :
+                    current_download_pages = failed_pdf_download
+                    log_and_print(f"{len(failed_pdf_download)} PDFs non téléchargés sur '{file_name}'. Nouvelle tentative.", logfile)
+            
+            # écrire tous les pdf récupérés dans un fichier
+            if len(pdf_binary_content) == len(download_pages) : 
+                write_binary_to_pdf(pdf_binary_content, file_path)
+                log_and_print(f"PDF fusionné et sauvegardé pour {file_name} : {file_path}", logfile)
+            else:
+                log_and_print(f"PDF incomplet pour {file_name} : {len(pdf_binary_content)}/{len(download_pages)} téléchargés", logfile)
+            article_end_time = time.perf_counter()
+            article_execution_time = article_end_time - article_start_time
+            log_and_print(f"\nDurée de traitement de l'article - {file_name} : {article_execution_time:.2f} secondes\n", logfile)
+            log_and_print("\n"+"#"*123+"\n", logfile)
+        
+        end_time = time.perf_counter()
+        execution_time = end_time - start_time
+
+        log_and_print(f"\nDURÉE TOTALE DE TRAITEMENT : {execution_time:.2f} secondes\n", logfile)
+        log_and_print("\n"+"="*51+"  FIN DE TRAITEMENT  "+"="*51+"\n", logfile)
+        
+
+
+###########################################################################
+
+
+if __name__ =="__main__" :
+    iterate_all_untill_all_downloaded(data)
+
